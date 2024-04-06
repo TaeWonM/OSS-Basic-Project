81efe564b255a2d6899e5456cf41a1659b5a0ecb
TaeWonM <minququ123456@gmail.com>
2024-04-06 14:41:14 +0900

    Add Open_Pose.py, Import와 각 파트 번호 설정

---
+++ Open_Pose.py
@@ -0,0 +1,14 @@
+# Import할 라이브러리들
+import cv2
+from pathlib import Path
+
+# MPII에서 각 파트 번호, 선으로 연결될 POSE_PAIRS
+BODY_PARTS = { "Head": 0, "Neck": 1, "RShoulder": 2, "RElbow": 3, "RWrist": 4,
+                "LShoulder": 5, "LElbow": 6, "LWrist": 7, "RHip": 8, "RKnee": 9,
+                "RAnkle": 10, "LHip": 11, "LKnee": 12, "LAnkle": 13, "Chest": 14,
+                "Background": 15 }
+
+POSE_PAIRS = [ ["Head", "Neck"], ["Neck", "RShoulder"], ["RShoulder", "RElbow"],
+                ["RElbow", "RWrist"], ["Neck", "LShoulder"], ["LShoulder", "LElbow"],
+                ["LElbow", "LWrist"], ["Neck", "Chest"], ["Chest", "RHip"], ["RHip", "RKnee"],
+                ["RKnee", "RAnkle"], ["Chest", "LHip"], ["LHip", "LKnee"], ["LKnee", "LAnkle"] ]
