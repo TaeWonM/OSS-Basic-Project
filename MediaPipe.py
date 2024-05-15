e8d60d494ba9a80330c06a9ddc659d831203dfea
TaeWonM <minququ123456@gmail.com>
2024-05-15 22:13:33 +0900

    Add MediaPipe.py and Add imports and settings

---
+++ MediaPipe.py
@@ -0,0 +1,8 @@
+# pip install mediapipe
+# define imports and settings
+import cv2
+import mediapipe as mp
+import numpy as np
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_pose = mp.solutions.pose
