bd2906e3e03bace1f41a5214617f02eed1c0d60e
TaeWonM <minququ123456@gmail.com>
2024-05-15 23:49:14 +0900

    Add Method to set arguments

--- MoveNet.py
+++ MoveNet.py
@@ -28,3 +28,29 @@ def get_args():
     args = parser.parse_args()
 
     return args
+
+#Method to run models
+def run_inference(model, input_size, image):
+    image_width, image_height = image.shape[1], image.shape[0]
+
+    input_image = cv.resize(image, dsize=(input_size, input_size))  # Revert size in WebCam
+    input_image = cv.cvtColor(input_image, cv.COLOR_BGR2RGB)  # Set image_color
+    input_image = input_image.reshape(-1, input_size, input_size, 3)  # reshape size in input_image
+    input_image = tf.cast(input_image, dtype=tf.int32)
+
+    outputs = model(input_image) #run models
+
+    keypoints_with_scores = outputs['output_0'].numpy()
+    keypoints_with_scores = np.squeeze(keypoints_with_scores)
+
+    keypoints = []
+    scores = []
+    for index in range(17):
+        keypoint_x = int(image_width * keypoints_with_scores[index][1])
+        keypoint_y = int(image_height * keypoints_with_scores[index][0])
+        score = keypoints_with_scores[index][2]
+
+        keypoints.append([keypoint_x, keypoint_y])
+        scores.append(score)
+
+    return keypoints, scores
