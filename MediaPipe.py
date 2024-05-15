f2344f07d1012330a136674fdbe817f0736c651f
TaeWonM <minququ123456@gmail.com>
2024-05-15 22:24:59 +0900

    Add OpenCv Webcam Setting

--- MediaPipe.py
+++ MediaPipe.py
@@ -6,3 +6,17 @@ import numpy as np
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
+
+# For webcam input:
+cap = cv2.VideoCapture(0)
+with mp_pose.Pose(
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as pose:
+  while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+      print("Ignoring empty camera frame.")
+      continue
+    if cv2.waitKey(1) & 0xFF == 27:
+      break
+cap.release()
