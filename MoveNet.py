afc4e394f09ba376ebe2e74f1653cafa4a0bc0cc
TaeWonM <minququ123456@gmail.com>
2024-05-15 23:42:17 +0900

    Add Method to Set Arguments

--- MoveNet.py
+++ MoveNet.py
@@ -10,3 +10,21 @@ import cv2 as cv
 import numpy as np
 import tensorflow as tf
 import tensorflow_hub as tfhub
+
+#Method to set arguments
+def get_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--device", type=int, default=0)
+    parser.add_argument("--file", type=str, default=None)
+    parser.add_argument("--width", help='cap width', type=int, default=960)
+    parser.add_argument("--height", help='cap height', type=int, default=540)
+
+    parser.add_argument('--mirror', action='store_true')
+
+    parser.add_argument("--model_select", type=int, default=0)
+    parser.add_argument("--keypoint_score", type=float, default=0.4)
+
+    args = parser.parse_args()
+
+    return args
