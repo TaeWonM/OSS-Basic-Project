7015d466f7c40775f3e7270610d057b2f09d3323
TaeWonM <minququ123456@gmail.com>
2024-05-15 23:40:40 +0900

    Add MoveNet.py and Set imports

---
+++ MoveNet.py
@@ -0,0 +1,12 @@
+#pip install tensorflow
+#pip install tensorflow_hub
+
+import sys
+import copy
+import time
+import argparse
+
+import cv2 as cv
+import numpy as np
+import tensorflow as tf
+import tensorflow_hub as tfhub
