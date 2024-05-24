7f71cdc28b6a0861e7a6c7269cf5435082b11796
TaeWonM <minququ123456@gmail.com>
2024-05-24 21:30:10 +0900

    Refactoring main in MediaPipe.py and Add sqaut statement in main

--- MediaPipe.py
+++ MediaPipe.py
@@ -123,34 +123,39 @@ def left_sqaut(result_list , movecount, stat) :
     return False
   except:
     return True
-
-cap = cv2.VideoCapture(0)
-with mp_pose.Pose(
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as pose:
-  while cap.isOpened():
-    success, image = cap.read()
-    if not success:
-      print("Ignoring empty camera frame.")
-      continue
+def main ():
+  cap = cv2.VideoCapture(0)
+  with mp_pose.Pose(
+      min_detection_confidence=0.5,
+      min_tracking_confidence=0.5) as pose:
+    while cap.isOpened():
+      success, image = cap.read()
+      if not success:
+        print("Ignoring empty camera frame.")
+        continue
     # To improve performance, optionally mark the image as not writeable to
     # pass by reference.
-    image.flags.writeable = False
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = pose.process(image)
+      image.flags.writeable = False
+      image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+      results = pose.process(image)
     # Draw the pose annotation on the image.
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    mp_drawing.draw_landmarks(
+      image.flags.writeable = True
+      image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+      mp_drawing.draw_landmarks(
         image,
         results.pose_landmarks,
         mp_pose.POSE_CONNECTIONS,
         landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
-    result_list = setdict(results, image.shape)
-    if (right_push_up(result_list, movecount, status) and right_push_up(result_list, movecount, status)):
-      print("No detacted")
+      result_list = setdict(results, image.shape)
+      if (right_push_up(result_list, movecount, status) and right_push_up(result_list, movecount, status)):
+        print("No Push_up pose detected")
+      if (right_sqaut(result_list,movecount,status) and left_sqaut(result_list,movecount,status)):
+        print("No sqaut pose detected")
     # Flip the image horizontally for a selfie-view display.
-    cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
-    if cv2.waitKey(1) & 0xFF == 27:
-      break
-cap.release()
+      cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
+      if cv2.waitKey(1) & 0xFF == 27:
+        break
+  cap.release()
+
+if __name__ == '__main__':
+    main()
