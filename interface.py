8c17819c36cbb093b8b81e252f3449c3aeb24b4c
TaeWonM <minququ123456@gmail.com>
2024-05-29 22:46:13 +0900

    Add interface.py and Add necessary imports

---
+++ interface.py
@@ -0,0 +1,13 @@
+import pygame
+from variable import window_height, window_width
+from pygame.locals import KEYUP
+import Charactor
+import set_map
+import container
+import sys
+import threading
+import MediaPipe
+import MoveNet
+import static
+import datetime as dt
+import random
