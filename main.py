9f74531bdc04c9c9183a3b3989ef62fcace4bf02
TaeWonM <minququ123456@gmail.com>
2024-05-29 21:47:00 +0900

    Add variable for change screens and models, Add main interface into current screen variable

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from pygame.locals import QUIT, KEYUP, K_ESCAPE
 import sys
+from variable import window_height, window_width
+import interface
 import static
 
 
@@ -14,6 +16,12 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((window_width, window_height))
     pygame.display.set_caption("H.G")
+    screen_stack = []
+    # to select models // 0 = Mediapipe // 1 = movenet
+    model_count = [0]
+    clock = pygame.time.Clock()
+    curent_screen = interface.main_screen(screen, body)
+    screen_stack.append(curent_screen)
 
 
 if __name__ == "__main__":
