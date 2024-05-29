0accfbb0ac8adb804cab79d96c26a1fc2ca1ec46
TaeWonM <minququ123456@gmail.com>
2024-05-30 00:48:00 +0900

    Add container.py and Add method to make blocks in setting interface

---
+++ container.py
@@ -0,0 +1,10 @@
+import pygame
+
+
+def setting_container():
+    container = []
+    container.append(pygame.Rect(280, 340, 32, 32))
+    container.append(pygame.Rect(280, 510, 32, 32))
+    container.append(pygame.Rect(280, 686, 32, 32))
+    container.append(pygame.Rect(280, 858, 32, 32))
+    return container
