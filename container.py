24ea39478826627b4be851077a2d84bd4efe3dd8
TaeWonM <minququ123456@gmail.com>
2024-05-30 00:54:39 +0900

    Add Heart class to make fight container

--- container.py
+++ container.py
@@ -1,6 +1,7 @@
 import pygame
 
 
+# method that make setting blocks list
 def setting_container():
     container = []
     container.append(pygame.Rect(280, 340, 32, 32))
@@ -8,3 +9,23 @@ def setting_container():
     container.append(pygame.Rect(280, 686, 32, 32))
     container.append(pygame.Rect(280, 858, 32, 32))
     return container
+
+
+# The class for heart
+# It contains Enemy's and player's heart
+# Also, It has parameters left, top, width, height, max_
+class heart:
+    def __init__(self, left, top, width, height, max_):
+        # variable to make left margin
+        self.left = left
+        # variable that contains how big heart bar is
+        self.width = width
+        # variable to make top margin
+        self.top = top
+        # variable to make left heart block
+        self.left_heart = pygame.Rect(left, top, width, height)
+        # variable to make attacked heart block
+        self.attacked_heart = pygame.Rect(left + width, top, 0, height)
+        # variable how much heart they have
+        self.max_heart = max_
+        self.cur_heart = max_
