b2b05829dcf88f81593e8a13bf64a88ec8270226
TaeWonM <minququ123456@gmail.com>
2024-05-29 21:35:17 +0900

    Add bodys variable for getting previous record

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import pygame
 from pygame.locals import QUIT, KEYUP, K_ESCAPE
 import sys
-def main () :
-    
+import static
+
+
+def main():
+    # 통계에 필요한 값들을 bodys 변수에 저장
+    bodys = static.get_statious()
+    static.nomalization_statious()
+    bodys = static.get_statious()
+
+
 if __name__ == "__main__":
     main()
