d015ba547d4022b0278a4543238a1d5669a24f41
TaeWonM <minququ123456@gmail.com>
2024-05-29 22:53:25 +0900

    Add main interface class and Add inital variables

--- interface.py
+++ interface.py
@@ -11,3 +11,24 @@ import MoveNet
 import static
 import datetime as dt
 import random
+
+
+# The class for main interface
+# It contains Enemys and players
+# Also, It has parameters screen, bodys
+# Screen is a pygame object, and it is a object to display game
+# body is a dictionary it contains today's date, month, year and record
+class main_screen:
+    def __init__(self, screen, bodys):
+        self.screen = screen
+        self.to_x = 0
+        self.to_y = 0
+        self.player = Charactor.Player()
+        self.background_group = pygame.sprite.Group()
+        self.move_interface_flag = False
+        self.bodys = bodys
+        self.player.rect.x = (window_width / 2) - (self.player.rect.size[0] / 2)
+        self.player.rect.y = window_height - self.player.rect.size[1]
+        self.enemy_index = 0
+        self.enemy = []
+        self.map_data = set_map.set_main_map(self.background_group)
