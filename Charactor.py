61e4f5002aee5f22fc1bb533b0696fec91df7fe5
TaeWonM <minququ123456@gmail.com>
2024-05-30 01:08:52 +0900

    Add method to make player's movement dynamically

--- Charactor.py
+++ Charactor.py
@@ -18,3 +18,25 @@ class Player(pygame.sprite.Sprite):
         self.movement_flag = True
         # variable to check frames how much they move
         self.movement_count = 0
+
+    # This is a method to make player's movements
+    def move_img(self):
+        if self.movement_count >= 5:
+            self.movement_count -= 5
+            if self.movement_flag:
+                self.movement += 1
+            else:
+                self.movement -= 1
+            if self.movement >= 2:
+                self.movement_flag = False
+            elif self.movement <= 0:
+                self.movement_flag = True
+            match (self.movement):
+                case 0:
+                    self.image = pygame.image.load("charactor1.png").convert_alpha()
+                case 1:
+                    self.image = pygame.image.load("charactor2.png").convert_alpha()
+                case 2:
+                    self.image = pygame.image.load("charactor3.png").convert_alpha()
+        else:
+            self.movement_count += 1
