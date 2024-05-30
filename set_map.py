2594f839c5848c70ad0efabccfe7f017d64ac433
TaeWonM <minququ123456@gmail.com>
2024-05-30 12:13:24 +0900

    Add method for main map in interface

--- set_map.py
+++ set_map.py
@@ -33,3 +33,23 @@ class Ground(pygame.sprite.Sprite):
         # Add rock tile position
         self.rect.x = self.grid_x
         self.rect.y = self.grid_y
+
+
+# This mothod is for setting map
+# Game reads map.txt and add tiles into background
+def set_main_map(background_group):
+    map_file = "map.txt"
+    map_data = []
+
+    with open(map_file, "r") as file:
+        for line in file:
+            map_data.append(line.strip("\n").split(" "))
+    for col in range(0, len(map_data)):
+        for row in range(0, len(map_data[col])):
+            if map_data[col][row] == "r":
+                rock = Rock(col, row)
+                background_group.add(rock)
+            elif map_data[col][row] == "g":
+                ground = Ground(col, row)
+                background_group.add(ground)
+    return map_data
