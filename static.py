4f853288a8d74ccd4d1cc93a3e97c351aacd0aea
TaeWonM <minququ123456@gmail.com>
2024-05-30 12:32:02 +0900

    Add statement to get recodes in staticsitc.json file and find current date

--- static.py
+++ static.py
@@ -9,4 +9,18 @@ def get_statious():
     now = dt.datetime.now()
     # variable that contains recodes which is in json file
     health_data = []
+    # variable to check current date
     flag = False
+    #Add try statement for exception 
+    try:
+        # statement to open staticsitc.json file
+        with open("staticsitc.json", "r", encoding="utf-8-sig") as json_file:
+            # statement to get recodes in staticsitc.json file and find current date
+            health_data = json.load(json_file)
+            for i in range(0, len(health_data)):
+                if (
+                    health_data[i]["year"] == now.year
+                    and health_data[i]["month"] == now.month
+                    and health_data[i]["day"] == now.day
+                ):
+                    flag = True
