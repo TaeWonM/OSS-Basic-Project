ca36e3dc63d8632f819bc53aa1628a99f8f96d2e
TaeWonM <minququ123456@gmail.com>
2024-05-30 12:11:06 +0900

    Add ground class for background

--- set_map.py
+++ set_map.py
@@ -10,9 +10,25 @@ class Rock(pygame.sprite.Sprite):
         # TILESIZE is 64px*64px
         self.grid_x = row * TILESIZE
         self.grid_y = col * TILESIZE
-        # variable for load image
+
         self.image = pygame.image.load("tile_rock.png").convert_alpha()
         # create block for tile position
+        self.rect = self.image.get_rect()
+        # Add rock tile position
+        self.rect.x = self.grid_x
+        self.rect.y = self.grid_y
+
+
+# This is class for background tiles
+# This tile looks like grass
+class Ground(pygame.sprite.Sprite):
+    def __init__(self, col, row):
+        pygame.sprite.Sprite.__init__(self)
+        # TILESIZE is 64px*64px
+        self.grid_x = row * TILESIZE
+        self.grid_y = col * TILESIZE
+        # variable for load image
+        self.image = pygame.image.load("tile_ground.png").convert_alpha()
         self.rect = self.image.get_rect()
         # Add rock tile position
         self.rect.x = self.grid_x
