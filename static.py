5bf559fb615ccfa47b231dc9a395760f4975c281
TaeWonM <minququ123456@gmail.com>
2024-05-30 12:28:03 +0900

    Add variable for get_statious method

--- static.py
+++ static.py
@@ -1,6 +1,12 @@
 import json
 import datetime as dt
 
-#This method makes json file for recode
-#It contains times and exercise recodes
+
+# This method makes json file for recode
+# It contains times and exercise recodes
 def get_statious():
+    # variable that contains time data
+    now = dt.datetime.now()
+    # variable that contains recodes which is in json file
+    health_data = []
+    flag = False
